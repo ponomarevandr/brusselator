7317eccf78bb412a876cc8eff0e308958c1d31b3
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2023-03-27 16:28:18 +0300

    A bug fixed in build.sh

--- visualizer/build.sh
+++ visualizer/build.sh
@@ -4,6 +4,7 @@ echo "==================== Checking build directory..."
 cd "$(dirname $0)"
 mkdir -p build
 cd build
+
 echo "==================== Setting path to Root..."
 if [ ! -f root_path.txt ];
 then
@@ -14,6 +15,7 @@ fi
 read -r ROOT_DIR < root_path.txt
 echo "From folder ${ROOT_DIR}"
 export ROOT_DIR
+
 echo "==================== Setting path to FLTK..."
 if [ ! -f fltk_path.txt ];
 then
@@ -23,7 +25,9 @@ then
 fi
 read -r FLTK_DIR < fltk_path.txt
 echo "From folder ${FLTK_DIR}"
+FLTK_DIR="${FLTK_DIR}/build"
 export FLTK_DIR
+
 echo "==================== Running CMake..."
 echo "With custom flags $1"
 CUSTOM_FLAGS="$1"
@@ -32,6 +36,7 @@ cmake ..
 if [ $? -ne 0 ]; then
 	exit 1
 fi
+
 echo "==================== Running Make..."
 make
 if [ $? -ne 0 ]; then
